ab5046a0a12ce7a75a0ceb12b612c8e50d3244e1
spring <spring.wind2006@163.com>
2026-05-28 08:04:56 +0800

    Add docs page

---
+++ docs/assets/site.js
@@ -0,0 +1,9 @@
+(function () {
+  var toggle = document.querySelector('.nav-toggle');
+  var nav = document.querySelector('.site-nav');
+  if (toggle && nav) {
+    toggle.addEventListener('click', function () {
+      nav.classList.toggle('open');
+    });
+  }
+})();
